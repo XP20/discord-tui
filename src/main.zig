1a70c1dc9a653360946f89b32b4e0995ee01c399
XP20 <67698795+XP20@users.noreply.github.com>
2025-05-21 20:05:53 +0300

    init

---
+++ src/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const discord = @import("discord");
+
+pub fn main() !void {
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try bw.flush(); // Don't forget to flush!
+}
+
+test "use other module" {
+    try std.testing.expectEqual(@as(i32, 150), discord.add(100, 50));
+}
